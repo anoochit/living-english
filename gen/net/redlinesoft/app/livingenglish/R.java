eef5075e130b4efec4e6c23a320eef43117eaaeb
redlinesoft <anoochit@gmail.com>
2013-02-17 12:05:40 +0700

    update

--- gen/net/redlinesoft/app/livingenglish/R.java
+++ gen/net/redlinesoft/app/livingenglish/R.java
@@ -21,9 +21,7 @@ public final class R {
         public static final int ColTitle=0x7f070002;
         public static final int linearLayout1=0x7f070000;
         public static final int listItem=0x7f070003;
-        public static final int mainLayout=0x7f070004;
-        public static final int menu_share=0x7f070006;
-        public static final int menu_update=0x7f070005;
+        public static final int mnurefresh=0x7f070004;
     }
     public static final class layout {
         public static final int activity_column=0x7f030000;
@@ -35,6 +33,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int menu_refresh=0x7f040007;
         public static final int menu_share=0x7f040004;
         public static final int menu_update=0x7f040002;
         public static final int text_share_body=0x7f040006;
--- src/net/redlinesoft/app/livingenglish/MainActivity.java
+++ src/net/redlinesoft/app/livingenglish/MainActivity.java
@@ -24,12 +24,13 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
@@ -39,112 +40,98 @@ public class MainActivity extends Activity {
   
 	String url = "http://query.yahooapis.com/v1/public/yql?q=select%20title%2Clink%20from%20feed%20where%20url%3D%22https%3A%2F%2Fgdata.youtube.com%2Ffeeds%2Fapi%2Fplaylists%2FSP0A00C7530C185317%3Fv%3D2%26max-results%3D50%22%20and%20link.rel%3D%22alternate%22&format=json&callback=";
  
-	int data_size = 0;
-	int attemp = 0;
-
+	int data_size = 0; 
 	public ArrayList<HashMap<String, String>> MyArrList = new ArrayList<HashMap<String, String>>();
 	public HashMap<String, String> map;
+	ListView listItem;
 	
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_main); 
+		setProgressBarIndeterminateVisibility(false);
+		
+		
 
 		// break policy
 		if (android.os.Build.VERSION.SDK_INT > 9) {
 			StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 					.permitAll().build();
 			StrictMode.setThreadPolicy(policy);
-		} 
-		
-		ListView listItem = (ListView) findViewById(R.id.listItem);	 
+		} 		
 	 
-		// TODO this is fucking thread		
-		if (checkNetworkStatus()) {			
-			
-			// loadcontent
-			loadContent();
-			
-			// check timeout
-			while ((data_size<0) & (attemp < 30)) {
-				try {
-					attemp=attemp+1;
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();					 
-				}
-			}
-			
-			if (data_size>0) {
-				LazyAdapter adapter = new LazyAdapter(this, MyArrList);
-				listItem.setAdapter(adapter);	  
-			} 		
-			
+		if (checkNetworkStatus()) {	 
+			new  LoadContentAsync().execute(); 			
 		} else {
 			Toast.makeText(getBaseContext(), "No network connection!",Toast.LENGTH_SHORT).show();
+		} 
+	}
+	
+	public class LoadContentAsync extends AsyncTask<String, Void, Void> {
+
+		@Override
+		protected Void doInBackground(String... params) {
+			// load json data
+			try {
+				JSONObject json_data = new JSONObject(getJSONUrl(url));
+				JSONObject json_query = json_data.getJSONObject("query");
+				JSONObject json_result = json_query.getJSONObject("results");
+				JSONArray json_entry = json_result.getJSONArray("entry");
+				Log.d("JSON", String.valueOf(json_entry.length()));
+
+				for (int i = 0; i < json_entry.length(); i++) {
+
+					// parse json
+					JSONObject c = json_entry.getJSONObject(i);
+					Log.d("JSON", c.getString("title").toString());
+					Log.d("JSON", c.getJSONObject("link").getString("href")
+							.toString());
+					String link = c.getJSONObject("link").getString("href")
+							.toString();
+					String[] fragments = link.split("&");
+					String[] videoid = fragments[0].split("=");
+					Log.d("JSON", videoid[1]);
+
+					String title_data = c.getString("title").toString();
+					String[] title_fragment = title_data.split("-");
+
+					// put into hashmap
+					map = new HashMap<String, String>();
+					map.put("title", title_fragment[1].toString().trim());
+					map.put("link", c.getJSONObject("link").getString("href"));
+					map.put("videoid", videoid[1]);
+					MyArrList.add(map);
+				}
+
+				data_size = json_entry.length();
+
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				Toast.makeText(getBaseContext(), "Cannot connect to server!",Toast.LENGTH_SHORT).show();
+			}
+			return null;
 		}
 
-		// OnClick Item
-		listItem.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				// TODO Auto-generated method stub
-				Intent fanPageIntent = new Intent(Intent.ACTION_VIEW);
-				fanPageIntent.setType("text/url");
-				fanPageIntent.setData(Uri
-						.parse(MyArrList.get(arg2).get("link")));
-				startActivity(fanPageIntent);
-			}
-		});
-
-	}
-
-	public void loadContent() {
-
-		// load json data
-		try {
-			JSONObject json_data = new JSONObject(getJSONUrl(url));
-			JSONObject json_query = json_data.getJSONObject("query");
-			JSONObject json_result = json_query.getJSONObject("results");
-			JSONArray json_entry = json_result.getJSONArray("entry");
-			Log.d("JSON", String.valueOf(json_entry.length()));
-
-			for (int i = 0; i < json_entry.length(); i++) {
-
-				// parse json
-				JSONObject c = json_entry.getJSONObject(i);
-				Log.d("JSON", c.getString("title").toString());
-				Log.d("JSON", c.getJSONObject("link").getString("href")
-						.toString());
-				String link = c.getJSONObject("link").getString("href")
-						.toString();
-				String[] fragments = link.split("&");
-				String[] videoid = fragments[0].split("=");
-				Log.d("JSON", videoid[1]);
-
-				String title_data = c.getString("title").toString();
-				String[] title_fragment = title_data.split("-");
-
-				// put into hashmap
-				map = new HashMap<String, String>();
-				map.put("title", title_fragment[1].toString().trim());
-				map.put("link", c.getJSONObject("link").getString("href"));
-				map.put("videoid", videoid[1]);
-				MyArrList.add(map);
-			}
-
-			data_size = json_entry.length();
-
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			Toast.makeText(getBaseContext(), "Cannot connect to server!",Toast.LENGTH_SHORT).show();
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			ShowResult(MyArrList);
+			setProgressBarIndeterminateVisibility(false);
 		}
 
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+			setProgressBarIndeterminateVisibility(true);
+		}
+		
 	}
+	 
+ 
 
 	public String getJSONUrl(String url) {
 		StringBuilder str = new StringBuilder();
@@ -174,32 +161,31 @@ public class MainActivity extends Activity {
 		return str.toString();
 	}
 
+	public void ShowResult(ArrayList<HashMap<String, String>> myArrList) { 
+		if (data_size>0) {
+			ListView listItem = (ListView) findViewById(R.id.listItem);	
+			LazyAdapter adapter = new LazyAdapter(this, MyArrList);
+			listItem.setAdapter(adapter);				
+			listItem.setOnItemClickListener(new OnItemClickListener() {
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+						long arg3) {
+					Intent fanPageIntent = new Intent(Intent.ACTION_VIEW);
+					fanPageIntent.setType("text/url");
+					fanPageIntent.setData(Uri.parse(MyArrList.get(arg2).get("link")));
+					startActivity(fanPageIntent);			
+				}
+			});
+		}
+	}
+
 	public boolean checkNetworkStatus() {
 		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo activeNetworkInfo = connectivityManager
 				.getActiveNetworkInfo();
 		return activeNetworkInfo != null;
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		/*
-		 * switch (item.getItemId()) { case R.id.menu_share: Log.d("MENU",
-		 * "select menu share"); Intent sharingIntent = new Intent(
-		 * android.content.Intent.ACTION_SEND); sharingIntent.setType("text/*");
-		 * sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-		 * getString(R.string.text_share_subject));
-		 * sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-		 * getString(R.string.text_share_body) +
-		 * getApplicationContext().getPackageName());
-		 * startActivity(Intent.createChooser(sharingIntent,
-		 * getString(R.string.menu_share))); startActivity(sharingIntent);
-		 * break; case R.id.menu_update: Log.d("MENU", "select menu update");
-		 * break; }
-		 */
-		return false;
-
-	}
+ 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
